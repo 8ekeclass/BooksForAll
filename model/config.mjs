7044d8d7c775938918f0775e7f1fda972a0f2677
8ekeclass <epal_epan_eclass@yahoo.gr>
2024-11-11 21:31:26 +0200

    new2 adds

--- model/config.mjs
+++ model/config.mjs
@@ -1,19 +1,19 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize, Op, Model, DataTypes} from 'sequelize';
 
 const sequelize = new Sequelize(
     {
         host: 'localhost',
         port: 5432,
         database: 'postgres',
-        schema: 'booklist',
+        schema: 'mybooklist',
         dialect: 'postgres',
-        username: 'booklistuser',
-        password: 'booklistuser',
+        //username: 'booklistuser',
+        //password: 'booklistuser',
         logging: false,
         define: {
             timestamps: false,
             freezeTableName: true
-        }
+        },
     });
 
 export default sequelize
